7e131ebd5c342e9219d94cbda9f966f9e6e6bfb1
Jay <dhananjay.shah244@gmail.com>
2023-06-19 18:24:48 +0200

    add oemt-bamf questionnaire automation.

---
+++ cypress/e2e/oet-bamf.cy.ts
@@ -0,0 +1,58 @@
+
+const baseUrl = 'https://oet.bamf.de/ords/oetut/f?p=514:1:15762836149516:::::';
+const startQuestion = 1;
+const endQuestion = 311;
+let counter: number = 1
+let arrayQuestionsDone: number[] = []
+
+
+function uniqueRandomQuestionId(arrayQuestionsDone: number[]): number {
+  let questionId =  randomQuestionId();
+  while (true) {
+    if (questionPerformed(questionId, arrayQuestionsDone)) {
+      questionId =  randomQuestionId();
+    }
+    else {
+      break;
+    }
+  }
+  return questionId
+}
+
+function randomQuestionId(): number {
+  return Math.floor(Math.random() * (endQuestion - startQuestion) + startQuestion)
+}
+
+function questionPerformed(questionId: number, arrayQuestionsDone: number[]): boolean {
+  for (var oneQuestionId of arrayQuestionsDone) {
+    if (oneQuestionId === questionId) {
+      // return true if question is already answered.
+      return true
+    }
+  }
+  return false;
+}
+
+// for (let id = 1; id <= 310; id++) {
+//   let questionId = uniqueRandomQuestionId(arrayQuestionsDone)
+//   arrayQuestionsDone.push(questionId);
+//   console.log(`questionId: ${questionId}`);
+// }
+
+
+describe('browse through questions', function () {
+  beforeEach(function () {
+    cy.visit(baseUrl);
+    cy.get('#P1_BUL_ID').select('Bayern');
+    cy.get('input[value="Zum Fragenkatalog"]').click();
+  });
+  context('Answer questions', function () {
+    for (let id = startQuestion; id <= endQuestion; id++) {
+      let questionId = uniqueRandomQuestionId(arrayQuestionsDone);
+      it(`Random question: ${questionId}`, function () {
+        cy.get('#P30_ROWNUM').select(`${questionId}`);
+        cy.contains('richtige Antwort =>').should('not.be.hidden');
+      });
+    }
+  });
+});
